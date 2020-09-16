3111a11044d7d2ecd028591414797da4a5e72d0c
10175423 <escvector@gmail.com>
2020-09-15 22:32:13 -0400

    helper files update

--- tfs.bat
+++ tfs.bat
@@ -4,10 +4,12 @@ SETLOCAL ENABLEEXTENSIONS
 SETLOCAL DISABLEDELAYEDEXPANSION
 
 REM set str=data.aws_region.info["eu-west-2"]
+REM module.regions.data.aws_regions.all:
 
 set str=%1
 REM echo.%str%
 set str=%str:"="""%
+set str=%str::=%
 REM echo.%str%
 tf state show %str%
 
